07d6644ee264ae0069c5e24e0123519dc9fa61d9
IsaakGroepT <Isaak@GroepT>
2015-12-27 13:21:22 +0100

    Schrijven() en lezen() zijn af

--- src/db/Bestand.java
+++ src/db/Bestand.java
@@ -4,9 +4,12 @@ import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
+import java.util.UUID;
 
-import application.Registration;
-import model.datum.Datum;
+import model.CD;
+import model.Film;
+import model.Item;
+import model.Spel;
 
 public class Bestand {
 
@@ -15,58 +18,67 @@ public class Bestand {
 	 */
 	public void opslaan()
 	{
-        try {
+		try {
 			PrintWriter OpslaanInBestand = new PrintWriter("items.txt");
-	        
-	        for (int n = 0; n < Uitleenlijst.getItems().size(); n++)
-	        {
-	        	OpslaanInBestand.println(registrations.get(n).formaatVoorBestandLijn());
-	        }
-	        OpslaanInBestand.close();
-	        
-	        return true;
-        }
-        catch (IOException e) {
-            System.out.println(e.toString());
-            return false;
-        }
+
+			for (int n = 0; n < Uitleenlijst.getItems().size(); n++) {
+				OpslaanInBestand.println(Uitleenlijst.getItem(n));
+			}
+			OpslaanInBestand.close();
+		} catch (IOException e) {
+			System.out.println(e.toString());
+		}
 	}
-	
+
 	/**
 	 * 
 	 */
 	public void lezen()
 	{
-		  File file = new File("items.txt");
-		  
-		  if (!file.exists())
-		  {
-			  return;
-		  }
-		  
-		  try {
-			Scanner scanner = new Scanner(file);
-			
-			while (scanner.hasNext())
-			{
+		File file = new File("items.txt");
+		Scanner scanner = null;
+
+		if (!file.exists()) {
+			return;
+		}
+
+		try {
+			scanner = new Scanner(file);
+			Item item;
+
+			while (scanner.hasNext()) {
 				String lijn = scanner.nextLine();
-				
+
 				// Leeg bestand
-				if (lijn.isEmpty())
-				{
+				if (lijn.isEmpty()) {
 					break;
 				}
+
+				String[] itemData = lijn.split(";");
 				
-							
+				//TODO: veranderen naar factory DP?
+				if (itemData[0] == "CD") {
+					item = new CD(itemData[2], UUID.fromString(itemData[1]));
+				}
+				else if (itemData[0] == "Film") {
+					item = new Film(itemData[2], UUID.fromString(itemData[1]));
+				}
+				else if (itemData[0] == "Spel") {
+					item = new Spel(itemData[2], UUID.fromString(itemData[1]));
+				}
+				else {
+					throw new Exception("Dit type item is niet bestaande");
+				}
+				Uitleenlijst.addItem(item);
 			}
-			
-			if (scanner != null)
-			{
+
+			if (scanner != null) {
 				scanner.close();
 			}
-		  }
-		  catch(Exception e) {
-			  System.out.println(e.getMessage());
-		  }
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		} finally {
+			scanner.close();
+		}
 	}
 }
