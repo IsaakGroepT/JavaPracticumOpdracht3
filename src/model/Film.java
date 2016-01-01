f86b4ff5bb94dbcf8efe364f39d45e5f99d0d772
IsaakGroepT <Isaak@GroepT>
2016-01-01 17:46:29 +0100

    Boete berekening

--- src/model/Film.java
+++ src/model/Film.java
@@ -4,9 +4,12 @@ import java.util.UUID;
 
 public class Film extends Item {
 	
-	final double prijs = 5;
-	final int dagenBasisprijsGeldig = 3;
-	final double prijsPerDagNaInbegrepenDagen = 2;
+	/*
+	 * Dus komt neer op: 5 euro voor drie dagen, voor elke dag daarna 2 euro per dag
+	 */
+	private final double prijs = 5;
+	private final int dagenBasisprijsGeldig = 3;
+	private final double prijsPerDagNaInbegrepenDagen = 2;
 	
 	public Film(String titel)
 	{
@@ -19,7 +22,31 @@ public class Film extends Item {
 	}
 	
 	/**
-	 * Aangepaste getPrijs() methode voor deze subklasse
+	 * Sommige items hebben een minimumperiode voor de prijs die ze betalen
+	 * 
+	 * @return
+	 */
+	public int getMinimumLeenPeriode()
+	{
+		return dagenBasisprijsGeldig;
+	}
+	
+	/**
+	 * Geeft gewoon de prijs terug. In dit geval (item is een film) wordt de prijs per dag
+	 * na 3 dagen gebruikt. Standaard zitten altijd 3 dagen inbegrepen voor een prijs van 5 euro.
+	 * Dus om de boete te berekenen moet eerst bekeken worden of het leentermijn > 3
+	 */
+	@Override
+	public double getPrijs()
+	{
+		return prijsPerDagNaInbegrepenDagen;
+	}
+	
+	/**
+	 * Geeft de prijs voor het item terug berekend voor het aantal dagen. 
+	 * 
+	 * @param aantalDagen
+	 * @return
 	 */
 	@Override
 	public double getPrijs(int aantalDagen)
