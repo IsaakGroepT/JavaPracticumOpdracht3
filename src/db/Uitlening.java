8e3a848d0a7e270c9127fb2d5993d442722cef7c
IsaakGroepT <Isaak@GroepT>
2016-01-03 22:59:59 +0100

    datumberekening was fout

--- src/db/Uitlening.java
+++ src/db/Uitlening.java
@@ -11,7 +11,7 @@ import model.DatumVerschil;
 public class Uitlening {
 	private UUID item;
 	private int klant;
-	private int aantalDagen;
+	private int aantalDagenBetaald;
 	private Datum startDatum;
 	private Datum eindDatum;
 	
@@ -19,7 +19,7 @@ public class Uitlening {
 	{
 		item = itemID;
 		klant = klantID;
-		aantalDagen = dagen;
+		aantalDagenBetaald = dagen;
 		
 		startDatum = new Datum();
 		eindDatum = new Datum();
@@ -38,7 +38,7 @@ public class Uitlening {
 
 	public int getAantalDagen()
 	{
-		return aantalDagen;
+		return aantalDagenBetaald;
 	}
 
 	public String getStartDatum()
@@ -53,11 +53,11 @@ public class Uitlening {
 	
 	public boolean isTeLaat() throws Exception
 	{
-		return new DatumVerschil(startDatum, eindDatum).getDagen() > aantalDagen;
+		return new DatumVerschil(startDatum, new Datum()).getDagen() > aantalDagenBetaald;
 	}
 	
 	public int getAantalDagenTeLaat() throws Exception
 	{
-		return new DatumVerschil(startDatum, eindDatum).getDagen() - aantalDagen;
+		return new DatumVerschil(eindDatum, new Datum()).getDagen();
 	}
 }
